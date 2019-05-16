6718d8bac92f5907d4c63a2124b3244b8ba8be13
Julia Yang <33442853+yangjulia@users.noreply.github.com>
2019-05-16 11:03:26 -0700

    finished paper

---
+++ Escape Room/Assets/Drawer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drawer : MonoBehaviour {
+	Animation anim;
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animation> ();
+	}
+
+	void Awake(){
+
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerStay(Collider other){
+		Debug.Log ("ok");
+		if (Input.GetKey ("f")) {
+			anim.Play ();
+		}
+	}
+
+
+}
--- Escape Room/Assets/Scripts/Paper.cs
+++ Escape Room/Assets/Scripts/Paper.cs
@@ -1,22 +1,56 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Paper : MonoBehaviour {
 
-	public Animation anim;
+	Animation anim;
+	AudioSource sound;
+	public GameObject obj;
+	Canvas can;
+
+	bool once = false;
 	// Use this for initialization
 	void Start () {
-		anim = GetComponent<Animation> ();
+		obj = GameObject.Find ("Paper Canvas");
 	}
 
 	void Awake() {
-		anim.wrapMode = WrapMode.Once;
-		anim.Play ();
+		sound = GetComponent<AudioSource> ();
+		anim = GetComponent<Animation> ();
+
+		can = obj.GetComponent<Canvas> ();
+
+		can.enabled = false;
+
+		foreach (AnimationState state in anim)
+		{
+			state.speed = 0.5F;
+		}
+
+		sound.Play ();
+
 	}
 
 	// Update is called once per frame
 	void Update () {
-		
+		if (!sound.isPlaying && !once) {
+			anim.Play ();
+			once = true;
+		}
+
 	}
+
+
+	void OnMouseDown(){
+		can.enabled = true;
+	}
+
+	public void ExitPaper(){
+		can.enabled = false;
+	}
+
+
+		
 }
