06db89637e0efafb211ecdfc33d3a27817e76daa
Julia Yang <33442853+yangjulia@users.noreply.github.com>
2019-05-20 11:56:55 -0700

    dialogue box

---
+++ Escape Room/Assets/Scripts/Dialogue.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dialogue : MonoBehaviour {
+
+	Text text;
+	// Use this for initialization
+	void Awake () {
+		text = GetComponent<Text> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown("b")){
+			text.text = "Why am I so short?";
+		}
+			
+	}
+}
